85fed70d9e3708bb7b1d9e355a52798f011d2d96
Shrawan-git <shrawanshaky3@gmail.com>
2021-01-12 15:25:20 +0545

    mute unmute

--- public/script.js
+++ public/script.js
@@ -82,3 +82,19 @@ const muteUnmute = () => {
       myVideoStream.getAudioTracks()[0].enabled = true;
     }
   }
+  
+const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
+  
+  const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+  }
