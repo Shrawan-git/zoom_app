3ef53a2ea5eb145b1157ded0a602ff69bd257d15
Shrawan-git <shrawanshaky3@gmail.com>
2021-01-12 13:34:55 +0545

    styling

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server)
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+    debug: true
+});
 const { v4: uuidv4 } = require('uuid');
 
+app.use('/peerjs', peerServer);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -17,9 +22,9 @@ app.get('/:room', (req, res) => {
 })
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId) => {
+    socket.on('join-room', (roomId, userId) => {
         socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected');
+        socket.to(roomId).broadcast.emit('user-connected', userId);
         })
 })
 
